e214bdec5de42694c145a2386db047322ce51f53
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-07-10 14:42:53 +0530

    added RxDart

--- lib/Bloc.dart
+++ lib/Bloc.dart
@@ -1,16 +1,26 @@
 import 'dart:async';
 import './blocs/Validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc extends Object with Validators {
-  final _emailController = StreamController<String>();
-  final _passwordController = StreamController<String>();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   Stream<String> get email => _emailController.stream.transform(validateEmail);
   Stream<String> get password =>
       _passwordController.stream.transform(validatePassword);
+  Stream<bool> get submitValid => Observable.combineLatest2(
+      email, password, (emailProvided, passwordProvided) => true);
 
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+
+  submit() {
+    final validEmail = _emailController.value;
+    final validPassword = _emailController.value;
+
+    print('Email $validEmail Password $validPassword');
+  }
 
   dispose() {
     _emailController.close();
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
 import '../Bloc.dart';
+import '../Provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Card(
       child: Container(
         child: Column(
           children: <Widget>[
-            emailField(),
-            passwordField(),
+            emailField(bloc),
+            passwordField(bloc),
             Container(
               margin: EdgeInsets.only(top: 15.0),
             ),
-            submitButton(),
+            submitButton(bloc),
           ],
         ),
         margin: EdgeInsets.all(15.0),
@@ -22,7 +25,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: (context, snapshot) {
@@ -39,7 +42,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: (context, snapshot) {
@@ -57,12 +60,17 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      onPressed: () {},
-      child: Text('Login'),
-      color: Colors.white,
-      textColor: Colors.black,
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          onPressed: snapshot.hasData ? bloc.submit : null,
+          child: Text('Login'),
+          color: Colors.white,
+          textColor: Colors.black,
+        );
+      },
     );
   }
 }
